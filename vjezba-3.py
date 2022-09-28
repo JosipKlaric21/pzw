34c5ef51d3d7875b1319a6de23a12b98922da671
JosipKlaric21 <114573069+JosipKlaric21@users.noreply.github.com>
2022-09-28 11:25:49 +0200

    Add files via upload

---
+++ vjezba-3.py
@@ -0,0 +1,15 @@
+ime=input("unesi svoje ime")
+print("pozdrav,"+ ime)
+
+a=int(input("unesi broj a:"))
+b=int(input("unesi broj b:"))
+
+
+zbroj=a+b
+print("Zbroj brojeva{0} i {1} iznosi {2}".format(a,b,zbroj))
+
+razlika=a-b
+print("Zbroj brojeva{0} i {1} iznosi {2}".format(a,b,razlika))
+
+kolicnik=a/b
+print("Zbroj brojeva{0} i {1} iznosi {2}".format(a,b,kolicnik))
